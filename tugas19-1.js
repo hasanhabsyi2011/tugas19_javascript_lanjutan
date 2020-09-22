1862e6e1102ece850fc2689b04347ffd2b5b78dc
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-09-22 07:51:50 +0700

    Add files via upload

---
+++ tugas19-1.js
@@ -0,0 +1,4 @@
+var ulang = 'Semangat...'
+var hasil2= ulang.repeat(5)
+
+console.log(hasil2)
---
+++ tugas19-2.js
@@ -0,0 +1,13 @@
+var bintang = '*'
+
+console.log(bintang.repeat(5))
+console.log(bintang.repeat(20))
+console.log(bintang.repeat(45))
+console.log(bintang.repeat(50))
+
+console.log(bintang.repeat(75))
+
+console.log(bintang.repeat(50))
+console.log(bintang.repeat(45))
+console.log(bintang.repeat(20))
+console.log(bintang.repeat(5))
